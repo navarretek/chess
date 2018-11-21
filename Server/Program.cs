79c07e323aa43393c9832fca3c98b306ca330130
Kevin Navarrete <knavarrete@dmacc.edu>
2018-11-21 15:56:22 -0800

    Initial Commit

---
+++ Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = ipHostInfo.AddressList[0];
+            TcpListener server = new TcpListener(ip,)
+        }
+    }
+}
