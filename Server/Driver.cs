1f0289ab4c5d7d26e80717229017e2bb224be7d0
Frank Navarrete <hippiewho@yahoo.com>
2018-11-24 21:18:03 -0800

    Server To Run On Seperate Thread

--- Server/Driver.cs
+++ Server/Driver.cs
@@ -5,18 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using Global.Models;
+
 namespace Server
 {
     class Driver
     {
         static void Main(string[] args)
         {
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
             IPAddress ip = ipHostInfo.AddressList[0];
-            TcpListener server = new TcpListener(ip, 8000);
 
 
+            Server s = new Server(ip, 8080);
+            s.StartServer();
+            s.Listen();
         }
     }
 }
--- Server/src/Server/Server.cs
+++ Server/src/Server/Server.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Server.src.Server
+namespace Server
 {
         
     class Server
@@ -15,6 +16,8 @@ namespace Server.src.Server
         private IPAddress _ipAddress;
         private int _port;
         private TcpListener tcpListener;
+        private Thread _listenThread;
+
         #endregion
 
         public String Port
@@ -37,6 +40,7 @@ namespace Server.src.Server
             try
             {
                 tcpListener = new TcpListener(_ipAddress, _port);
+                Console.WriteLine($"Starting Server on IP: {_ipAddress.ToString()} : {_port}");
                 tcpListener.Start();
                 return true;
             }
@@ -46,13 +50,69 @@ namespace Server.src.Server
             }
         }
 
+        public void StopServer()
+        {
+            try
+            {
+                Console.WriteLine($"Stoping Server on IP: {_ipAddress.MapToIPv4()} : {_port}");
+                // If listener is not null then stop
+                tcpListener?.Stop();
+            }
+            catch 
+            {
+            }
+        }
+
         // TODO: Start listening to incoming connections
         /// <summary>
         /// Start listening for connections.
         /// </summary>
-        public void Listen()
+        public void Listen(bool forceRestart = false)
         {
+            if (_listenThread == null)
+            {
+                _listenThread = new Thread(ListeningThreadFunction);
+                _listenThread.Start();
+            }
+            else if (forceRestart)
+            {
+                Console.WriteLine("Restarting Listener");
+                _listenThread.Abort();
+                Thread.Sleep(5000);            
+                Listen();
+            }
+        }
 
+        public void StopListening()
+        {
+            if (_listenThread != null &&_listenThread.IsAlive)
+            {
+                _listenThread.Abort();
+            }
+        }
+
+
+        private void ListeningThreadFunction()
+        {
+            try
+            {
+                Console.Write("Listening...");
+                while (true)
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    byte[] r = new byte[100];
+
+                    stream.Read(r, 0, r.Length);
+
+                    Console.Write(Encoding.ASCII.GetString(r , 0, r.Length));
+
+                }
+            }
+            catch 
+            {
+                //ignore
+            }
         }
     }
 }
